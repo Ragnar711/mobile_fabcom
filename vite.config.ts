da606cdd571a42e24fc458b23c1e5ac2c02ecbba
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2024-10-23 14:37:36 +0100

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "ARAMES",
+                short_name: "ARMS",
+                description: "Your app description",
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                display: "standalone",
+                scope: "/",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "./icons/manifest-icon-192.maskable.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "./icons/manifest-icon-512.maskable.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
+});
